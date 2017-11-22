339a4ece3edb6f8f3af084c7aa3c699526e434ce
Kathir2312 <kathiregb@gmail.com>
2017-11-22 15:58:56 +0400

    Add project files.

---
+++ Bot ApplicationV1/Dialogs/RootDialog.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System.Collections.Generic;
+
+namespace Bot_ApplicationV1.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            // calculate something for us to return
+            int length = (activity.Text ?? string.Empty).Length;
+
+            // return our reply to the user
+            await context.PostAsync($"You sent {activity.Text} which was {length} characters");
+
+            var reply = context.MakeMessage();
+            reply.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+            reply.Attachments = new List<Microsoft.Bot.Connector.Attachment>();
+            List<string> lst = new List<string>();
+            lst.Add("KathiresanP\n");
+            lst.Add("\n" + activity.Text);
+            reply.AddHeroCard(
+                string.Empty,
+                lst
+                );
+
+            await context.PostAsync(reply);
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
---
+++ Bot ApplicationV1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bot_ApplicationV1.WebApiApplication" Language="C#" %>
